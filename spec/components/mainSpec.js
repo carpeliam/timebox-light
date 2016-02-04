56f5b5bc46b04d99b1b235d86d4b86c9491b6803
Liam Morley <liam@carpeliam.com>
2016-02-03 23:59:50 -0800

    add ES Lint, EditorConfig
    - installs [es6 AirBnB style
    guide](https://github.com/airbnb/javascript), including React - run via
    `npm run lint`
    - fixes most offending issues against style guide, but leaves a few TBD
    (not sure what we’d want to do about react/jsx-no-bind)
    - sets spacing to 2 spaces, uses EditorConfig file to help editors use
    corresponding settings

--- spec/components/mainSpec.js
+++ spec/components/mainSpec.js
@@ -1,54 +1,68 @@
-import React from 'react'
-import TestUtils from 'react-addons-test-utils'
-import Main from '../../src/assets/components/Main'
-import Box from '../../src/assets/components/Box'
+/* eslint-env jasmine */
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import Main from '../../src/assets/components/Main';
+import Box from '../../src/assets/components/Box';
 
 describe('Main', () => {
-    let main, box;
-    beforeEach(() => {
-        main = TestUtils.renderIntoDocument(<Main></Main>);
-        box = TestUtils.findRenderedComponentWithType(main, Box);
+  let main;
+  let box;
+  beforeEach(() => {
+    main = TestUtils.renderIntoDocument(<Main />);
+    box = TestUtils.findRenderedComponentWithType(main, Box);
+  });
+
+  it('contains a box', () => {
+    expect(box).toBeDefined();
+  });
+
+  it('passes props to box', () => {
+    expect(box.props.name).toEqual('I am a box');
+    expect(box.props.duration).toEqual(0);
+    expect(box.props.lastCheckedTime).toBeUndefined();
+    expect(box.props.paused).toBeTruthy();
+  });
+
+  it('starts out in a paused state', () => {
+    expect(main.state.paused).toBeTruthy();
+  });
+
+  it('clears timer on unmount', () => {
+    const clearIntervalSpy = spyOn(global, 'clearInterval');
+    main.componentWillUnmount();
+    expect(clearIntervalSpy).toHaveBeenCalled();
+  });
+
+  it('should not increase the duration if paused', () => {
+    jasmine.clock().install();
+    jasmine.clock().mockDate();
+    main = TestUtils.renderIntoDocument(<Main />);
+
+    jasmine.clock().tick(100);
+    expect(main.state.duration).toEqual(0);
+    jasmine.clock().tick(100);
+    expect(main.state.duration).toEqual(0);
+    jasmine.clock().uninstall();
+  });
+
+  describe('when clicking a button', () => {
+    it('changes state of paused', () => {
+      TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithTag(main, 'button'));
+      expect(main.state.paused).toBeFalsy();
     });
 
-    it('contains a box', () => {
-        expect(box).toBeDefined();
+    it('starts a timer', () => {
+      jasmine.clock().install();
+      jasmine.clock().mockDate();
+      main = TestUtils.renderIntoDocument(<Main />);
+      TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithTag(main, 'button'));
+      jasmine.clock().tick(100);
+      expect(main.state.duration).toEqual(0);
+      expect(main.state.lastCheckedTime).toEqual(Date.now());
+      jasmine.clock().tick(100);
+      expect(main.state.duration).toEqual(100);
+      expect(main.state.lastCheckedTime).toEqual(Date.now());
+      jasmine.clock().uninstall();
     });
-
-    it('passes props to box', () => {
-        expect(box.props.name).toEqual('I am a box');
-        expect(box.props.duration).toEqual(0);
-        expect(box.props.lastCheckedTime).toBeUndefined();
-        expect(box.props.paused).toBeTruthy();
-    });
-
-    it('has state of paused', () => {
-        expect(main.state.paused).toBeTruthy();
-    });
-
-    it('clears timer on unmount', () => {
-        const clearIntervalSpy = spyOn(global, 'clearInterval');
-        main.componentWillUnmount();
-        expect(clearIntervalSpy).toHaveBeenCalled();
-    });
-
-    describe('when clicking a button', () => {
-        it('changes state of paused', () => {
-            TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithTag(main, 'button'));
-            expect(main.state.paused).toBeFalsy();
-        });
-
-        it('starts a timer', () => {
-            jasmine.clock().install();
-            jasmine.clock().mockDate();
-            main = TestUtils.renderIntoDocument(<Main></Main>);
-            TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithTag(main, 'button'));
-            jasmine.clock().tick(100);
-            expect(main.state.duration).toEqual(0);
-            expect(main.state.lastCheckedTime).toEqual(Date.now());
-            jasmine.clock().tick(100);
-            expect(main.state.duration).toEqual(100);
-            expect(main.state.lastCheckedTime).toEqual(Date.now());
-            jasmine.clock().uninstall();
-        });
-    });
+  });
 });
--- src/assets/components/Box.js
+++ src/assets/components/Box.js
@@ -2,27 +2,23 @@ import React from 'react';
 import moment from 'moment';
 import 'moment-duration-format';
 
-class Box extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+export default class Box extends React.Component {
+  formattedTime() {
+    return moment.duration(this.props.duration).format('h:mm:ss', { trim: false });
+  }
 
-    formattedTime() {
-        return moment.duration(this.props.duration).format('h:mm:ss', {trim: false});
-    }
-
-    handlePause() {
-        this.props.onStart()
-    }
+  handlePause() {
+    this.props.onStart();
+  }
 
 
-    render() {
-        const pauseState = this.props.paused ? 'Start' : 'Pause';
-        return <div>
-            {this.props.name} {this.formattedTime()}
-            <button onClick={this.handlePause.bind(this)}>{pauseState}</button>
-        </div>;
-    }
+  render() {
+    const pauseState = this.props.paused ? 'Start' : 'Pause';
+    return (
+      <div>
+        {this.props.name} {this.formattedTime()}
+        <button onClick={this.handlePause.bind(this)}>{pauseState}</button>
+      </div>
+    );
+  }
 }
-
-export default Box;
